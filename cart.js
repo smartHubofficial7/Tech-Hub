3db6fc5e23341c74b21b1a4ae7cf14c4b869953d
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 15:01:33 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,27 +1,39 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || []
+let cart=JSON.parse(localStorage.getItem("cart"))||[]
 
 let div=document.getElementById("cart-items")
 
-if(cart.length===0){
+let total=0
 
-div.innerHTML="<h3>Your cart is empty</h3>"
+cart.forEach((item,index)=>{
 
-}else{
-
-cart.forEach(item=>{
+total+=item.price
 
 div.innerHTML+=`
 <div class="cart-item">
+
 <h3>${item.name}</h3>
 <p>₹${item.price}</p>
+
+<button onclick="removeItem(${index})">Remove</button>
+
 </div>
 `
 
 })
 
+document.getElementById("total").innerText="Total: ₹"+total
+
+function removeItem(i){
+
+cart.splice(i,1)
+
+localStorage.setItem("cart",JSON.stringify(cart))
+
+location.reload()
+
 }
 
-function goCheckout(){
+function checkout(){
 
 window.location.href="checkout.html"
 
