a25e64a6434ea76027ab8c3cb14c02225ba782ee
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 14:42:12 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,7 +1,11 @@
-let cart=JSON.parse(localStorage.getItem("cart"))||[];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let div = document.getElementById("cart-items");
+div.innerHTML = "";
 
-let div=document.getElementById("cart-items");
-
-cart.forEach(item=>{
-div.innerHTML+=`<p>${item.name} - ₹${item.price}</p>`;
-});
+if(cart.length === 0){
+    div.innerHTML = "<p>Your cart is empty</p>";
+} else {
+    cart.forEach(item => {
+        div.innerHTML += `<p>${item.name} - ₹${item.price}</p>`;
+    });
+}
