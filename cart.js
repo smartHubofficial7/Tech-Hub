e01bf9be0e7aa57a0da52dea29ab9828c43f2317
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 14:36:04 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,69 +1,7 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || []
+let cart=JSON.parse(localStorage.getItem("cart"))||[];
 
-const cartDiv = document.getElementById("cartItems")
+let div=document.getElementById("cart-items");
 
-function showCart(){
-
-cartDiv.innerHTML=""
-
-if(cart.length===0){
-
-cartDiv.innerHTML="<h2 style='text-align:center'>Your cart is empty</h2>"
-
-document.getElementById("totalPrice").innerText=""
-
-return
-
-}
-
-let total=0
-
-cart.forEach((p,i)=>{
-
-total += Number(p.price)
-
-cartDiv.innerHTML += `
-
-<div class="card">
-
-<img src="${p.image}">
-
-<h3>${p.name}</h3>
-
-<p>₹${p.price}</p>
-
-<button onclick="removeItem(${i})">Remove</button>
-
-</div>
-
-`
-
-})
-
-document.getElementById("totalPrice").innerText="Total: ₹"+total
-
-}
-
-showCart()
-
-function removeItem(i){
-
-cart.splice(i,1)
-
-localStorage.setItem("cart",JSON.stringify(cart))
-
-showCart()
-
-}
-
-function checkout(){
-
-window.location.href="checkout.html"
-
-}
-
-function goBack(){
-
-window.location.href="index.html"
-
-}
+cart.forEach(item=>{
+div.innerHTML+=`<p>${item.name} - ₹${item.price}</p>`;
+});
