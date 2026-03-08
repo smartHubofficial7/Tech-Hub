71952fd8089a4d0ad7f25c4ff52b3b6333496af2
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 14:41:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-function addCart(){
+// Cart array from localStorage
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
-alert("Product Added to Cart");
-
+// Add to Cart function
+function addToCart(name, price) {
+    cart.push({name, price});
+    localStorage.setItem("cart", JSON.stringify(cart));
+    alert(name + " added to cart!");
 }
 
-function buy(){
-
-alert("Proceed to Buy");
-
-}
+// Buy buttons functionality
+document.querySelectorAll(".buy").forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+        const product = document.querySelectorAll(".product")[index];
+        const name = product.querySelector("h3").innerText;
+        const price = parseInt(product.querySelector("p").innerText.replace("₹",""));
+        addToCart(name, price);
+        window.location.href = "checkout.html";
+    });
+});
