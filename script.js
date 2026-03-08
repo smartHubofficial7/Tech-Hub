7fa1bc3aafcbd51713b0b7e7e3e8849ed4bb7b76
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 13:51:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,80 @@
-function checkout(){
+let cart = [];
 
-let user = localStorage.getItem("user")
+function login(){
 
-if(!user){
-alert("Please login first")
-showPage("login")
-return
+let name = document.getElementById("name").value;
+let phone = document.getElementById("phone").value;
+
+if(name=="" || phone==""){
+
+alert("Enter details");
+
+return;
+
 }
 
-showPage("checkout")
+localStorage.setItem("userName",name);
+localStorage.setItem("phone",phone);
+
+document.getElementById("loginBox").style.display="none";
+document.getElementById("store").style.display="block";
+
+}
+
+function addToCart(name,price){
+
+cart.push({name,price});
+
+showCart();
+
+}
+
+function showCart(){
+
+let cartDiv=document.getElementById("cart");
+
+cartDiv.innerHTML="";
+
+cart.forEach(item=>{
+
+cartDiv.innerHTML+=`<p>${item.name} - ₹${item.price}</p>`;
+
+});
+
+}
+
+function order(){
+
+let address=document.getElementById("address").value;
+
+let name=localStorage.getItem("userName");
+
+let phone=localStorage.getItem("phone");
+
+if(address==""){
+
+alert("Enter address");
+
+return;
+
+}
+
+let message="New Order%0A";
+
+message+="Name: "+name+"%0A";
+
+message+="Phone: "+phone+"%0A";
+
+message+="Address: "+address+"%0A";
+
+message+="Items:%0A";
+
+cart.forEach(item=>{
+
+message+=item.name+" - ₹"+item.price+"%0A";
+
+});
+
+window.open("https://wa.me/918481944109?text="+message);
 
 }
