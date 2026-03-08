591a98bc05d51014a01641862bf2d93c262e4dc6
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 14:06:16 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,1 +1,35 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || []
 
+const cartDiv = document.getElementById("cartItems")
+
+function showCart(){
+
+cartDiv.innerHTML=""
+
+cart.forEach(p => {
+
+cartDiv.innerHTML += `
+
+<div class="card">
+
+<img src="${p.image}">
+
+<h3>${p.name}</h3>
+
+<p>₹${p.price}</p>
+
+</div>
+
+`
+
+})
+
+}
+
+showCart()
+
+function checkout(){
+
+window.location.href="checkout.html"
+
+}
