f37af232a40c3db6061089593d97ea1accddc79f
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-08 15:00:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
+let cart = JSON.parse(localStorage.getItem("cart")) || []
+
+updateCartCount()
 
 function addToCart(name,price){
 
 cart.push({name,price})
 
 localStorage.setItem("cart",JSON.stringify(cart))
+
+updateCartCount()
 
 alert(name+" added to cart")
 
@@ -15,5 +19,27 @@ function buyNow(name,price){
 addToCart(name,price)
 
 window.location.href="checkout.html"
+
+}
+
+function updateCartCount(){
+
+document.getElementById("count").innerText=cart.length
+
+}
+
+function searchProduct(){
+
+let input=document.getElementById("search").value.toLowerCase()
+
+let products=document.querySelectorAll(".product")
+
+products.forEach(p=>{
+
+let name=p.querySelector("h3").innerText.toLowerCase()
+
+p.style.display=name.includes(input)?"block":"none"
+
+})
 
 }
